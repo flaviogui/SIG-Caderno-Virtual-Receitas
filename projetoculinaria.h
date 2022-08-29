e9cd7c5614bd24177dbd1e84b385a518aeb13e9b
flaviogui <flavioglaydson-pc@hotmail.com>
2022-08-29 14:08:42 -0300

    Criação da tela principal

---
+++ projetoculinaria.h
@@ -0,0 +1,28 @@
+#include <stdio.h>
+
+void tela_principal(void);
+
+
+int main(void){
+    tela_principal();
+    
+    
+    return 0;    
+}
+
+void tela_principal(void){
+printf("_____________________________________________________________\n");
+printf("|-=-=-=-BEM VINDO AO CADERNO ELETRONICO DE RECEITAS-=-=-=-=-|\n");
+printf("|-=-=-=-=-=--=-=-=-(1) MENU ALIMENTOS-=-=-=-=-=-=-=-=-=-=-=-|\n");
+printf("|-=-=-=-=-=-=-=-=-=(2) MENU RECEITAS-=-=-=-=-=-=-=-=-=-=-=-=|\n");
+printf("|-=-=-=-=-=-=-=-=-=(3) MENU SOBRE-=-=-=-=-=-=-=-=-=-=-=-=-=-|\n");
+printf("|-=-=-=-=-=-=-=-=-=(4) MENU EQUIPE-=-=-=-=-=-=-=-=-=-=-=-=-=|\n");
+printf("|-=-=-=-=-=-=-=-=-=(5) MENU RELATORIOS-=-=-=-=-=-=-=-=-=-=-=|\n");
+printf("|-=-=-=-=-=-=-=-=-=(6) SAIR -=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=|\n");
+printf("|___________________________________________________________|\n");
+
+
+}
+
+
+
