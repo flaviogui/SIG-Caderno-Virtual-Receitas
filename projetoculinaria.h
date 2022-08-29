7e82c8e3625a63a912cc0726f5c12a7757e62b0e
flaviogui <flavioglaydson-pc@hotmail.com>
2022-08-29 15:06:27 -0300

    Criação da tela equipe

--- projetoculinaria.h
+++ projetoculinaria.h
@@ -2,11 +2,13 @@
 
 void tela_principal(void);
 void tela_sobre(void);
+void tela_equipe(void);
 
 
 int main(void){
     tela_principal();
     tela_sobre();
+    tela_equipe();
     
     return 0;    
 }
@@ -26,6 +28,7 @@ printf("|___________________________________________________________|\n");
 
 
 void tela_sobre(void) {
+printf("");
 printf("|||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||\n");
 printf("| -=- -=- =-= -=-   INFORMAÇÃO DO PROJETO   -=- -=- -=- -=-   |\n");
 printf("|-------- CADERNO VIRTUAL DE RECEITAS CULINARIAS -----------  |\n");
@@ -36,6 +39,19 @@ printf("|domésticos necessitam de receitas para conduzir a preparação |\n");
 printf("|assim, o presente projeto propõe um sistema capaz de gerencia|\n"); 
 printf("|um conjunto de receitas culinárias, permitindo a busca por   |\n");
 printf("|pratos que contenham determinados ingredientes e a respectiva|\n");
-printf("|otimização dos estoques.                                      |\n");                       
+printf("|otimização dos estoques.                                     |\n");
+printf("|||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||\n");                       
+
+}
+
+void tela_equipe(void) {
+printf("");
+printf("||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||\n"); 
+printf("| -=- -=- =-=    INFORMACOES SOBRE A EQUIPE     -=- -=- -=-  |\n"); 
+printf("| -=- FLAVIO GLAYDSON G. LOPES  ----- MATRICULA: 20220046917 |\n");
+printf("| -=- MATHEUS QUIRINO F. FIQUEIREDO - MATRICULA: 20220061520 |\n");
+printf("| =-= Somos alunos do curso de Sistema de Informação - UFRN  |\n");
+printf("| -=- DATA DO INICIO DO PROJETO: 29/08/2022 -=-              |\n");
+printf("||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||\n");
 
 }
